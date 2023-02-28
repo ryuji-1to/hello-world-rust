50f7b18acd746a1a4cd32e790e881760d7645048
ryuji-1to <ryuji0222ss@icloud.com>
2023-02-28 09:52:32 +0900

    update: 🌱 logic

--- src/main.rs
+++ src/main.rs
@@ -107,7 +107,53 @@ mod test {
     }
 
     #[tokio::test]
-    async fn should_find_todo() {}
+    async fn should_find_todo() {
+        let expected = Todo::new(1, "should_find_todo".to_string());
+        let repository = TodoRepositoryForMemory::new();
+        repository.create(CreateTodo::new("should_find_todo".to_string()));
+        let req = build_todo_req_with_empty(Method::GET, "/todos/1");
+        let res = create_app(repository).oneshot(req).await.unwrap();
+        let todo = res_to_todo(res).await;
+        assert_eq!(expected, todo);
+    }
+
+    #[tokio::test]
+    async fn should_get_all_todos() {
+        let expected = Todo::new(1, "should_get_all_todos".to_string());
+        let repository = TodoRepositoryForMemory::new();
+        repository.create(CreateTodo::new("should_get_all_todos".to_string()));
+        let req = build_todo_req_with_empty(Method::GET, "/todos");
+        let res = create_app(repository).oneshot(req).await.unwrap();
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let body: String = String::from_utf8(bytes.to_vec()).unwrap();
+        let todo: Vec<Todo> = serde_json::from_str(&body)
+            .expect(&format!("cannot convert Todo instance. body:{}", body));
+        assert_eq!(vec![expected], todo);
+    }
+
+    #[tokio::test]
+    async fn should_update_todo() {
+        let expected = Todo::new(1, "should_update_todo".to_string());
+        let repository = TodoRepositoryForMemory::new();
+        repository.create(CreateTodo::new("before_update_todo".to_string()));
+        let req = build_todo_req_with_json(
+            "/todos/1",
+            Method::PATCH,
+            r#"{"id":1,"text":"should_update_todo","completed":false}"#.to_string(),
+        );
+        let res = create_app(repository).oneshot(req).await.unwrap();
+        let todo = res_to_todo(res).await;
+        assert_eq!(expected, todo);
+    }
+
+    #[tokio::test]
+    async fn should_delete_todo() {
+        let repository = TodoRepositoryForMemory::new();
+        repository.create(CreateTodo::new("should_delete_todo".to_string()));
+        let req = build_todo_req_with_empty(Method::DELETE, "/todos/1");
+        let res = create_app(repository).oneshot(req).await.unwrap();
+        assert_eq!(StatusCode::NO_CONTENT, res.status());
+    }
 }
 
 // #[tokio::test]
